460e7d496879434009683e52a82647901d73ab02
Sajjon <alex.cyon@gmail.com>
2015-03-27 17:07:40 +0100

    Initial Commit

---
+++ BlockKVO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlockKVO
+//
+//  Created by Cyon Alexander (Ext. Netlight) on 27/03/15.
+//  Copyright (c) 2015 com.test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
